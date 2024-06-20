e4e2aa1c7455f159db14846bb806ec0975172923
ZackVazquez070407 <160301429+ZackVazquez070407@users.noreply.github.com>
2024-06-20 08:30:21 -0600

    base de datos

---
+++ aplicacion1_usuarios.sql
@@ -0,0 +1,53 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jun 20, 2024 at 04:29 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `prueba`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `aplicacion1_usuarios`
+--
+
+CREATE TABLE `aplicacion1_usuarios` (
+  `no_control` int(11) NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `turno` varchar(20) NOT NULL,
+  `correo` varchar(254) NOT NULL,
+  `contrasena` varchar(100) NOT NULL,
+  `rol` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `aplicacion1_usuarios`
+--
+ALTER TABLE `aplicacion1_usuarios`
+  ADD PRIMARY KEY (`no_control`),
+  ADD UNIQUE KEY `correo` (`correo`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
